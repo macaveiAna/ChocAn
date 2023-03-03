a20417c50e8f39641fb1ae780a0649d2cb0b95b3
mahadghani <mghani@pdx.edu>
2023-03-02 16:46:23 -0800

    new code

--- ChocAn.py
+++ ChocAn.py
@@ -11,12 +11,132 @@ class Manager:
     def check_member_status(self):
         pass
 
+class Member:
+ 
+    def __init__(self):
+        self.member_id = ""
+        self.member_name = ""
+     
+   
+    #first check in main function if member already exits
+    def add_member(self, member_id, member_name):
+        today = date.today()
+        #print(today)
+       
+        with open("Member/MemberDirectory.json", "r") as file:
+            data = json.load(file)
+        new_member = {
+            "MemberName": member_name, 
+            "MemberId": member_id, 
+            "Status": "Active", 
+            "last_payment": str(today)
+            }
+        data["members"].append(new_member)
+        with open("Member/MemberDirectory.json", "w") as file:
+            json.dump(data,file, indent = 4)
+        
+    def printSuspended(self):
+        print("Fees are owed. Member suspended...")
+
+    def setIfSuspended(self, member_id):
+        #today = date.today() + timedelta(days=30)
+
+        with open("Member/MemberDirectory.json", "r") as file:
+            data = json.load(file)
+        
+        for member in data['members']:
+            if member['MemberId'] == member_id:
+                date = strptime(member["last_payment"], "%Y/%m/%d")
+                date = date + timedelta(days=30)
+
+                if date.today() > date:
+                    # suspend this account
+                    member['Status'] = 'Suspended'
+                    self.printSuspended()
+                    return False
+                else:
+                    return True
+
+        return False # default return. If trouble finding account
+        
+
+    
+    #function to read in user input
+    def validate_member(self, member_id):
+        with open('Member/MemberDirectory.json') as file:
+            data = json.load(file)
+        
+        for member in data['members']:
+            if member['MemberId'] == member_id:
+                status = member['Status']
+                if status == 'Active':
+                    return self.setIfSuspended(member_id)
+                elif status == 'Suspended':
+                    return False
+        
+        return False
+    
+    def pay_monthly_fee(self):
+        pass
+        
+    #def remove_member(self):
+        #pass
+        
+        
+        
+    #function to compare what is in the file
+
+
 class Provider:
     def __init__(self):
         self.provider_id = ""
         self.provider_name = ""
-    #pass
+    
+    def getProviderID(self):
+        print("\nPlease enter a valid provider ID number.")
+        id = input("> ")
 
+        if len(id) != 9 or id.isnumeric() == False:
+            return self.getProviderID()
+        else:
+            return id
+
+    def getProviderName(self,id):
+        with open("Provider/ProviderList.json",mode="r") as file:
+            for line in file:
+                parts = line.strip().split(":")
+                if parts[0] == id:
+                    return parts[1]
+
+    def getMemberID(self):
+        print("\nPlease enter a valid member ID number.")
+        id = input("> ")
+        if len(id) != 9 or id.isnumeric() == False:
+            return self.getMemberID()
+        else:
+            return id
+
+    def load(self):
+        self.provider_id = self.getProviderID()
+        self.provider_name = self.getProviderName(self.provider_id)
+        print("Welcome ", self.provider_name)
+        m = Member()
+        m.member_id = self.getMemberID()
+        #got member contents to print from json file
+        #now we need to figure out how to check the status and
+        #charge the member depending if they are suspended or not
+        
+        var = m.validate_member(m.member_id)
+        
+        if var == True:
+            print("Validated")
+            # call function that does rest of work: self.load_validated()
+        else:
+            print("Invalid Number")
+            
+
+
+'''
 class Member:
  
     def __init__(self):
@@ -47,12 +167,12 @@ class Member:
     #function to read in user input
     def validate_member(self, member_id):
         with open('Member/MemberDirectory.json') as file:
-            '''
+            
             for line in file:
                 parts = line.strip().split(" ")
                 if parts[0] == member_id:
                     return True
-            '''
+            
             data = json.load(file)
         
         for member in data['members']:
@@ -75,7 +195,9 @@ class Member:
         
         
     #function to compare what is in the file
-    
+'''
+
+
 
 class Services:
     pass
@@ -100,23 +222,6 @@ class Terminal:
             self.type = "Manager"
 
         return self.type
-
-    def getProviderID(self):
-        print("\nPlease enter a valid provider ID number.")
-        id = input("> ")
-
-        if len(id) != 9 or id.isnumeric() == False:
-            return self.getProviderID()
-        else:
-            return id
-
-    def getProviderName(self,id):
-
-        with open("Provider/ProviderList.txt",mode="r") as file:
-            for line in file:
-                parts = line.strip().split(":")
-                if parts[0] == id:
-                    return parts[1]
         
     def getMemberID(self):
         print("\nPlease enter a valid member ID number.")
@@ -135,27 +240,9 @@ class Terminal:
         self.setType(choice)
 
         if self.type == "Provider": # if type is provider
-            #provider code
-            provider = Provider() # dec
-            provider.provider_id = self.getProviderID()
-            provider.provider_name = self.getProviderName(provider.provider_id)
-            print("Welcome ",provider.provider_name)
-            m = Member()
-            m.member_id = self.getMemberID()
-            #got member contents to print from json file
-            #now we need to figure out how to check the status and
-            #charge the member depending if they are suspended or not
-            m.validate_member(m.member_id)
-            '''
-            if m.validate_member(m.member_id) == True:
-                print("Validated")
-            else:
-                print("Invalid Number")
-            '''
-       
+            provider = Provider()
+            provider.load()
             
-            
-
         else: # if type is manager
-            #manager code
-            pass
+            manager = Manager()
+            manager.load()
