9b145234c07b0d6caa36ab1b1e64329a149670b7
macaveiAna <83480446+macaveiAna@users.noreply.github.com>
2023-03-02 22:35:26 -0700

    changes made

--- ChocAn.py
+++ ChocAn.py
@@ -152,6 +152,15 @@ class Provider:
         print("Is this the correct service that was provided? ","'",name,"'","[y/n]")
         ans = input("> ")
         if(ans == 'y'):
+            print("Would you like to enter comments about the service provided? [y/n]")
+            ans2 = input("> ")
+            if ans2 == 'y':
+                print("Please enter a comment (100 characters): ")
+                comment = input("> ")
+                filename = f"{self.provider_name}.json"
+                with open(filename, "w") as file:
+                    json.dump(comment, file)
+                
             with open("Services/ProviderDirectory.json") as file:
                 data = json.load(file)
             for service in data['services']:
@@ -169,7 +178,7 @@ class Provider:
             print("Service: ", name)
             self.validateServiceName(name)
 
-        
+      
     def load_validated(self):
         print("Please enter the date the service was provided:")
         date_service = input("> ")
