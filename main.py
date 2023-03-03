a272e312ea27b516dd1d73f7ef8ef4cefad3f029
Pooja <pooja3@pdx.edu>
2023-03-03 10:59:02 -0800

    deleted

--- main.py
+++ main.py
@@ -4,21 +4,13 @@
 
 #import ChocAn as object
 from ChocAn import *
-from LLL import *
 
 def main():
     # Creates var 'terminal' to hold Terminal object
     terminal = Terminal()
-    #member = Member()
-    
-    #list = LLL()
-    #print("\n\nHere are the existing members: \n")
-    #list.load()
-    #list.display()
-    
     terminal.loadTerminal()
     
-    #member.add_member("1234", "hi")
+    
     
 
 if __name__ == "__main__":
