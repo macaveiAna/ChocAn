f07367c2e9550061496ebd09dbe8aa5af1a56b2e
macaveiAna <83480446+macaveiAna@users.noreply.github.com>
2023-03-03 22:30:31 -0700

    created test_provider.py

--- test_ChocAn.py
+++ test_ChocAn.py
@@ -1,28 +1,10 @@
 import pytest
 from ChocAn import Terminal
-import Provider
+
 
 
 #Unit Test file for ChocAn functions
-obj1 = Provider.Provider()
-#when testing, please run with pytest -s to test output
-def test_enter_Provider_details():
-    obj1.enter_Provider_details()
-    
-    #check if it's not empty
-    assert obj1.provider_name != ""
-    assert obj1.provider_name != None
-    assert obj1.provider_id != ""
-    assert obj1.provider_id != None
-    assert obj1.strAddr != ""
-    assert obj1.strAddr != None
-    assert obj1.city != ""
-    assert obj1.city != None
-    assert obj1.state != ""
-    assert obj1.state != None
-    assert obj1.zip != ""
-    assert obj1.zip != None
-    
+ 
 #Test the type for the terminal
 # Need to figure out how to do in one test
 def test_setType(setup: Terminal):
