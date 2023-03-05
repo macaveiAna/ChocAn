85ad23948a519967ac6995dfecf1909647190757
Melissa Fielman <melissafielman@gmail.com>
2023-03-04 16:58:04 -0800

    add_provider & add_member updated

--- Member/Member.py
+++ Member/Member.py
@@ -2,35 +2,53 @@ import json
 import datetime
 from datetime import date, timedelta
 from time import strptime
-
+import os
+from pathlib import Path
 
 class Member:
  
     def __init__(self):
         self.member_id = ""
         self.member_name = ""
-        '''
         self.strAddr = ""
         self.city = ""
         self.state = ""
         self.zip = ""
-        '''
-   
+    
+
     #first check in main function if member already exits
-    def add_member(self, member_id, member_name):
+    def add_member(self):
         today = date.today()
         #print(today)
-       
-        with open("Member/MemberDirectory.json", "r") as file:
-            data = json.load(file)
         new_member = {
-            "MemberName": member_name, 
-            "MemberId": member_id, 
+            "MemberName": self.member_name, 
+            "MemberId": self.member_id, 
             "Status": "Active", 
             "last_payment": str(today)
             }
+        
         data["members"].append(new_member)
-        with open("Member/MemberDirectory.json", "w") as file:
+        cwd = os.getcwd() #gets current working directory
+        parent_dir = "Member/" #sets relative path in variable
+        
+        #If member already has a file, will print a statement that it exists
+        if os.path.exists(f"{cwd}/{parent_dir}/{self.member_name}"): 
+            self.print_exists()
+        else:
+            #creates the new member directory
+            directory = f"{self.member_name}/" #new member directory
+            '''
+            p = Path(f"/ChocAn/Provider/{directory}") #sets path to the new provider's directory
+            os.makedirs(p) #makes the directory
+            '''
+            path = os.getcwd() + "/Member/" + directory
+            os.makedirs(path)
+
+            #Appends the new member to the full member list
+        with open(f"{cwd}/{parent_dir}/MemberDirectory.json",mode="r") as file:
+            data = json.load(file)
+  
+        with open(f"{cwd}/{parent_dir}/MemberDirectory.json",mode="w") as file:
             json.dump(data,file, indent = 4)
         
     def printSuspended(self):
