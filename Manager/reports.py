52ca036d066210b8f6df63cda90d4dead4b29b45
Pooja <pooja3@pdx.edu>
2023-03-08 22:48:38 -0800

    finished create_member_weekly_reports

--- Manager/reports.py
+++ Manager/reports.py
@@ -1,12 +1,21 @@
 import json
+import datetime
 class reports:
     def __init__(self):
         pass
 
     def create_member_weekly_reports(self):
+        today = datetime.date.today()
         with open("Member/MemberDirectory.json", mode="r") as memberFile:
             all_members = json.load(memberFile)
         for member in all_members["members"]:
             name = member["MemberName"]
-            with open(f"Member/{name}/{name}_profile.json") as aMember:
-                pass
+            with open(f"Member/{name}/{name}_profile.json", "r") as aMemberFile:
+                aMember = json.load(aMemberFile)
+            with open(f"Member/{name}/{name}_{today}", "w") as f:
+                json.dump(aMember, f)
+            del aMember['Services']
+            with open(f"Member/{name}/{name}_profile.json", "w") as file:
+                json.dump(aMember,file)
+    
+    
