403153acd5fb34aac324290cd306d96e2252d10f
Pooja <pooja3@pdx.edu>
2023-03-08 22:27:57 -0800

    started reports class

--- Manager/reports.py
+++ Manager/reports.py
@@ -1,3 +1,12 @@
-
+import json
 class reports:
-    pass
+    def __init__(self):
+        pass
+
+    def create_member_weekly_reports(self):
+        with open("Member/MemberDirectory.json", mode="r") as memberFile:
+            all_members = json.load(memberFile)
+        for member in all_members["members"]:
+            name = member["MemberName"]
+            with open(f"Member/{name}/{name}_profile.json") as aMember:
+                pass
