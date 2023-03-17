e5fce536f342fce4a892cd591646bbd2ac861d1d
macaveiAna <83480446+macaveiAna@users.noreply.github.com>
2023-03-17 12:09:04 -0600

    last changes

--- ChocAn.py
+++ ChocAn.py
@@ -13,13 +13,13 @@ class Terminal:
     #test
     def getInitInput(self):
         options = {
-                'WELCOME TO CHOCAN': ['Options','1. If you are a Provider', '2. If you are a Manager']
+                'WELCOME TO CHOCAN': ['Options','1. If you are a Provider', '2. If you are a Manager', '3. Quit']
             }
         df = pd.DataFrame(options)
         df_styled = df.style.set_table_styles([{'selector': 'th', 'props': [('text-align', 'center')]}]).set_properties(**{'text-align': 'left'})
         print(tabulate(df_styled.data, headers=df_styled.columns, tablefmt='fancy_grid', showindex=False))
         choice = input("Please enter your choice: ")
-        if choice.isnumeric() == False or (int(choice) != 1 and int(choice) != 2):
+        if choice.isnumeric() == False or (int(choice) != 1 and int(choice) != 2 and int(choice) != 3):
             if os.name == 'nt':  # For Windows
                 os.system('cls')
             else:  # For Linux/Mac
@@ -32,25 +32,30 @@ class Terminal:
     def setType(self, choice):
         if choice == 1:
             self.type = "Provider"
-        else:
+        elif choice == 2:
             self.type = "Manager"
-
+        else:
+            self.type = None
         return self.type
         
     #test  
     def loadTerminal(self):
         choice = self.getInitInput()
-        self.setType(choice)
-        
-        if os.name == 'nt':  # For Windows
-            os.system('cls')
-        else:  # For Linux/Mac
-            os.system('clear')
+        while choice != 3:
+            self.setType(choice)
+            
+            if os.name == 'nt':  # For Windows
+                os.system('cls')
+            else:  # For Linux/Mac
+                os.system('clear')
 
-        if self.type == "Provider": # if type is provider
-            provider = Provider()
-            provider.load()
+            if self.type == "Provider": # if type is provider
+                provider = Provider()
+                provider.load()
+            
+            else: # if type is manager
+                manager = Manager()
+                manager.load()
+               
+            choice = self.getInitInput()
         
-        else: # if type is manager
-            manager = Manager()
-            manager.load()
